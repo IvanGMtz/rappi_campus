83f123de85ed3844ecb8fe83a7982a14ef5dd048
LauraRamirezCampus <lauravramirez0327@gmail.com>
2023-08-28 22:42:16 -0500

    feat: :sparkles: Add metod post for catalogo
    v1 to post the catalogue

--- app/routes/catalogo.js
+++ app/routes/catalogo.js
@@ -4,14 +4,18 @@ import express from "express";
 import {getUsuarioV1} from "../controllers/v1/usuario.js"
 import routesVersioning  from 'express-routes-versioning';
 import {validarToken} from "../middlewares/JWT.js";
-import { getCatalogoV1 } from "../controllers/v1/catalogo.js";
+import { getCatalogoV1, postCatalogoV1 } from "../controllers/v1/catalogo.js";
 
 const version = routesVersioning();
-const appUsuario = express.Router();
-appUsuario.use(validarToken);
+const appCatalogo= express.Router();
+appCatalogo.use(validarToken);
 
-appUsuario.get("/", version({
+appCatalogo.get("/", version({
     "^1.0.0": getCatalogoV1
 })); 
 
-export default appUsuario;
+appCatalogo.post("/", version({
+    "^1.0.0": postCatalogoV1
+})); 
+
+export default appCatalogo;

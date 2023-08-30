9bb214f9ba4f65a8c697154dc817ef89892efe9a
IvanGMtz <navi.garces24@gmail.com>
2023-08-29 21:47:37 -0500

    feat: :sparkles: Update endpoint empresa and add version 2
    CRUD cpmplete

--- app/routes/empresa.js
+++ app/routes/empresa.js
@@ -1,8 +1,8 @@
 import express from "express";
-import {getUsuarioV1} from "../controllers/v1/usuario.js"
 import routesVersioning  from 'express-routes-versioning';
 import {validarToken} from "../middlewares/JWT.js";
-import { getEmpresaV1 } from "../controllers/v1/empresa.js";
+import { getEmpresaV1, updateEmpresaById } from "../controllers/v1/empresa.js";
+import {postEmpresa, deleteEmpresaById} from "../controllers/v2/empresa.js";
 
 const version = routesVersioning();
 const appEmpresa = express.Router();
@@ -11,5 +11,17 @@ appEmpresa.use(validarToken);
 appEmpresa.get("/", version({
     "^1.0.0": getEmpresaV1
 })); 
+
+appEmpresa.put("/:id", version({
+    "^1.0.0": updateEmpresaById
+})); 
+
+appEmpresa.post("/", version({
+    "~2.0.1": postEmpresa
+}));
+
+appEmpresa.delete("/:id", version({
+    "~2.0.1": deleteEmpresaById
+}));
 
 export default appEmpresa;

fc0264652a576d8deeeb8b5a8a0735bca0973a7b
LauraRamirezCampus <lauravramirez0327@gmail.com>
2023-08-30 14:22:49 -0500

    feat: :sparkles: Add endpoint rappiTendero
    the post version 1 of rappiTendero

--- app/controllers/v1/rappiTendero.js
+++ app/controllers/v1/rappiTendero.js
@@ -1,3 +1,5 @@
+import { body, validationResult } from 'express-validator';
+import { siguienteId } from '../../helpers/counter.js';
 import {con} from "../../../config/connection/atlas.js";
 
 let db = await con();
@@ -8,4 +10,42 @@ export const getRappiTenderoV1 = async (req, res)=>{
     res.send(result);
     
 }
+
+export const postRappiTenderoV1 =async(req,res)=>{
+    try {
+        await Promise.all([
+            body('nombre').notEmpty().run(req),
+            body('apellido').notEmpty().run(req),
+            body('email').isEmail().run(req),
+            body('departamento').notEmpty().run(req),
+            body('ciudad').notEmpty().run(req),
+            body('direccion').notEmpty().run(req),
+            body('telefono').isNumeric().run(req),
+        ]);
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const nuevoProducto = {
+            id: await siguienteId("rappiTendero"),
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            email: req.body.email,
+            departamento: req.body.departamento,
+            ciudad: req.body.ciudad,
+            direccion: req.body.direccion,
+            telefono: req.body.telefono,
+            rol:3
+        };
+
+        const result = await collection.insertOne(nuevoProducto);
+        res.status(201).json({ message: "rappiTendero added successfully", insertedId: result.insertedId });
+    } catch (error) {
+        res.status(500).json({ message: "Error adding rappiTendero", error: error.message });
+    }
+};
+
+
 
--- app/routes/rappiTendero.js
+++ app/routes/rappiTendero.js
@@ -1,14 +1,18 @@
 import express from "express";
 import routesVersioning  from 'express-routes-versioning';
 import {validarToken} from "../middlewares/JWT.js";
-import { getRappiTenderoV1} from "../controllers/v1/rappiTendero.js";
+import { getRappiTenderoV1, postRappiTenderoV1} from "../controllers/v1/rappiTendero.js";
 
 const version = routesVersioning();
 const apprappiTendero = express.Router();
-apprappiTendero.use(validarToken);
+// apprappiTendero.use(validarToken);
 
 apprappiTendero.get("/", version({
     "^1.0.0": getRappiTenderoV1
+})); 
+
+apprappiTendero.post("/", version({
+    "^1.0.0": postRappiTenderoV1
 })); 
 
 export default apprappiTendero;

909ec219c872d94fab502df48cad9c598c3a8b8d
LauraRamirezCampus <lauravramirez0327@gmail.com>
2023-08-29 22:30:36 -0500

    feat: :sparkles: Add endPOint empresa

--- app/routes/empresa.js
+++ app/routes/empresa.js
@@ -2,7 +2,7 @@ import express from "express";
 import {getUsuarioV1} from "../controllers/v1/usuario.js"
 import routesVersioning  from 'express-routes-versioning';
 import {validarToken} from "../middlewares/JWT.js";
-import { getEmpresaV1 } from "../controllers/v1/empresa.js";
+import { getEmpresaV1, postEmpresaV1 } from "../controllers/v1/empresa.js";
 
 const version = routesVersioning();
 const appEmpresa = express.Router();
@@ -10,6 +10,10 @@ appEmpresa.use(validarToken);
 
 appEmpresa.get("/", version({
     "^1.0.0": getEmpresaV1
+})); 
+
+appEmpresa.post("/", version({
+    "^1.0.0": postEmpresaV1
 })); 
 
 export default appEmpresa;

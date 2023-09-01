34b7f9003af097dc0d04ed07a1fff221216d9db1
LauraRamirezCampus <lauravramirez0327@gmail.com>
2023-09-01 11:25:29 -0500

    feat: :sparkles: add endpoint factura
    post version 1 of factura

--- app/controllers/v1/factura.js
+++ app/controllers/v1/factura.js
@@ -17,4 +17,39 @@ export const getFacturaV1 = async (req, res) => {
 };
 
 
+export const postFacturaV1 =async(req,res)=>{
+    try {
+        await Promise.all([
+            body('id_cliente').notEmpty().isInt().run(req),
+            body('id_empresa').notEmpty().isInt().run(req),
+            body('id_rappiTendero').notEmpty().isInt().run(req),
+            body('fecha').isDate().run(req),
+            body('id_producto').isArray({}).run(req),
+        
+        ]);
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const nuevafactura = {
+            id: await siguienteId("factura"),
+            id_cliente: req.body.id_cliente,
+            id_empresa: req.body.id_empresa,
+            id_rappiTendero: req.body.id_rappiTendero,
+            fecha: req.body.fecha,
+            id_producto: req.body.id_producto,
+            
+        };
+
+        const result = await collection.insertOne(nuevafactura);
+        res.status(201).json({ message: "factura added successfully", insertedId: result.insertedId });
+    } catch (error) {
+        res.status(500).json({ message: "Error adding factura", error: error.message });
+    }
+};
+
+
+
 
--- app/routes/factura.js
+++ app/routes/factura.js
@@ -1,7 +1,7 @@
 import express from "express";
 import routesVersioning  from 'express-routes-versioning';
 import {validarToken} from "../middlewares/JWT.js";
-import { getFacturaV1 } from "../controllers/v1/factura.js";
+import { getFacturaV1, postFacturaV1 } from "../controllers/v1/factura.js";
 
 
 const version = routesVersioning();
@@ -10,4 +10,8 @@ appFactura.use(validarToken);
  
 appFactura.get("/", version({
     "^1.0.0": getFacturaV1
+})); 
+
+appFactura.post("/", version({
+    "^1.0.0": postFacturaV1
 })); 
